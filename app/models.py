0386c70e198998b946457f81b974740da62b0d42
Marcelo Luna <marcelo_luna2003@hotmail.com>
2024-06-27 00:39:11 -0300

    creando la clase avatar

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -43,5 +44,9 @@ class Inquilino(models.Model):
                    - {self.telefono}
                    - {self.mail}
                    - {self.valor_alquiler}'''
+    
 
+class Avatar(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)    
+    imagen = models.ImageField(upload_to='avatares', blank=True, null=True)
 
